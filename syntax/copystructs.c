929c8b61c1597a58371a9f090028baa8f657455b
Ray Allan <fierlion@gmail.com>
2015-09-06 08:39:47 -0700

    pointer in struct

--- syntax/copystructs.c
+++ syntax/copystructs.c
@@ -11,7 +11,17 @@ typedef struct {
 int main () {
   demo_s d1 = {.b=1, .c=2, .d=3, .efg=(int[]){4,5,6}};
   demo_s d2 = d1;
-
   assert(d1.b==1);
   assert(d2.efg[0]==4);
+
+  // change to d1 doesn't affect d2--d2 is a copy
+  d1.b = 24;
+  assert(d1.b==24);
+  assert(d2.b==1);
+  // but the copy of the pointer still points to the original data
+  d1.efg[0] = 523;
+  assert(d2.efg[0]==523);
+
+  // so if your struct contains no pointers there is no need for
+  // a struct copy function.
 }
