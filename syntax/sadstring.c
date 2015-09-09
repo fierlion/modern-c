d95ba23c4216dbf3ee17f1cb33dabae910e683a9
Ray Allan <fierlion@gmail.com>
2015-09-08 18:42:49 -0700

    adding sadstring

---
+++ syntax/sadstring.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+//
+// void get_strings(char const *in) {
+//   char *cmd;
+//   int len = strlen("strings ") + strlen(in) + 1;
+//   cmd = malloc(len);
+//   snprintf(cmd, len, "strings %s", in);
+//   if (system(cmd)) fprintf(stderr, "something went wrong running %s.\n", cmd);
+//   free(cmd);
+// }
